b8df62ca3cecc59f5730dfee2174df0da358bdbb
Ardikaas <ardikaajisetiawan@gmail.com>
2024-08-17 19:11:33 +0700

    fix responsive

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
 import About from "../components/About/About";
 import Detail from "../components/Detail/Detail";
 import Footer from "../components/Footer/Footer";
 import Home from "../components/Home/Home";
 
 const Landing = () => {
+  useEffect(() => {
+    document.title = "Undanga Pernikahan | Nova & Rizki";
+  }, []);
   return (
     <div>
       <Home />
